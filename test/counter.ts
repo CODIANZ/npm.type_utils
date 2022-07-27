36e7de9d9022d1541facd447a457dc24835dd8ae
Terukazu Inoue <terukazu.inoue@codianz.com>
2022-07-27 13:31:45 +0900

    Implemented Counter by removing Number.
    And changed the implementation of Repeat to Counter.

---
+++ test/counter.ts
@@ -0,0 +1,17 @@
+import { Decrement, Increment, IsSame, Repeat, Counter, static_assert, Value } from "../src";
+
+static_assert<IsSame<Value<Counter<0>>, 0>>(true);
+static_assert<IsSame<Value<Counter<1>>, 1>>(true);
+static_assert<IsSame<Value<Counter<500>>, 500>>(true);
+
+type C100 = Counter<100>;
+type C101 = Increment<C100>;
+type C099 = Decrement<C100>;
+
+static_assert<IsSame<Value<C100>, 100>>(true);
+static_assert<IsSame<Value<C101>, 101>>(true);
+static_assert<IsSame<Value<C099>,  99>>(true);
+
+static_assert<IsSame<Repeat<5, true>, [true, true, true, true, true]>>(true);
+static_assert<IsSame<Repeat<2, [true, 1]>, [[true, 1], [true, 1]]>>(true);
+
