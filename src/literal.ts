b6bd6bf01d0e3d9035b029830063483da2a45492
Terukazu Inoue <terukazu.inoue@codianz.com>
2022-08-07 20:26:21 +0900

    refactoring literal

--- src/literal.ts
+++ src/literal.ts
@@ -1,20 +1,33 @@
-export type Str<T> = T extends string ? `${T}` : never;
+export type LiterableTypes = string | number | bigint | boolean | null | undefined;
+export type PropertyKeyTypes = string | number | symbol;
 
-export type Concat<T1, T2, SEPARATOR = "."> = 
+export type Concat<
+  T1 extends LiterableTypes,
+  T2 extends LiterableTypes,
+  SEPARATOR extends string | undefined = undefined
+> = 
   T1 extends undefined ?
-    T2 extends undefined ?
-      never : `${Str<T2>}`
-  :
-    T2 extends undefined ?
-      never : `${Str<T1>}${Str<SEPARATOR>}${Str<T2>}`;
-  
-type DeepKeyOfMain<OBJ, KEY, SEPARATOR, OBJSTR> =
-  OBJ extends object ?
-    KEY extends keyof OBJ ?
+    T2 extends undefined ? never : `${T2}`
+  : T2 extends undefined ?
+    `${T1}` 
+    : SEPARATOR extends undefined ?
+      `${T1}${T2}` : `${T1}${SEPARATOR}${T2}`;
+
+type DeepKeyOfMain<
+  OBJ extends object,
+  KEY extends PropertyKeyTypes,
+  SEPARATOR extends string,
+  OBJSTR extends string | undefined
+> =
+  KEY extends keyof OBJ ?
+    KEY extends string | number ?
       OBJ[KEY] extends object ?
-      DeepKeyOfMain<OBJ[KEY], keyof OBJ[KEY], SEPARATOR, Concat<OBJSTR, KEY, SEPARATOR>>
+        DeepKeyOfMain<OBJ[KEY], keyof OBJ[KEY], SEPARATOR, Concat<OBJSTR, KEY, SEPARATOR>>
         : Concat<OBJSTR, KEY, SEPARATOR>
       : never
     : never;
 
-export type DeepKeyOf<T, SEPARATOR = "."> = DeepKeyOfMain<T, keyof T, SEPARATOR, undefined>;
+export type DeepKeyOf<
+  T extends object,
+  SEPARATOR extends string = "."
+> = DeepKeyOfMain<T, keyof T, SEPARATOR, undefined>;
