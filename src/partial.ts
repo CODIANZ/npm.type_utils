aed68103b44ebeceb3ba0ab29a154ef1553f2676
terukazu inoue <terukazu.inoue@codianz.com>
2024-02-07 17:38:34 +0900

    add `PartialBy`

--- src/partial.ts
+++ src/partial.ts
@@ -1,3 +1,4 @@
 export type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]>; } : T;
 export type RemovePartial<T> = { [P in keyof T]-?: T[P]; };
 export type DeepRemovePartial<T> = T extends object ? { [P in keyof T]-?: DeepRemovePartial<T[P]>; } : T;
+export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
